5e3e5da4ce9ae75ba9d27ce167d2bbd9d112c7b8
solutionspelichet <solutions.pelichet@gmail.com>
2026-03-13 11:47:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 
 window.APP_CONFIG = {
   APP_NAME: "Label Generator",
-  GOOGLE_CLIENT_ID: "REMPLACE_PAR_TON_CLIENT_ID_OAUTH",
+  GOOGLE_CLIENT_ID: "solutions.pelichet@gmail.com",
   DEFAULT_DRIVE_FOLDER_ID: "",
   MAX_ROWS: 300,
   DEFAULT_DPI: 200,
   DEFAULT_SHARE_PUBLICLY: false
 };
+
